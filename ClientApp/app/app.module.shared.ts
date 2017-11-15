e4144ca91f221ddaadea1f6c0e82067a21723583
stvn100 <31903922+stvn100@users.noreply.github.com>
2017-11-15 10:48:58 -0600

    Removed FetchData template component

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -7,7 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ContactService } from './components/services/ContactService';
 
@@ -16,7 +15,6 @@ import { ContactService } from './components/services/ContactService';
         AppComponent,
         SidebarComponent,
         ContactComponent,
-        FetchDataComponent,
         HomeComponent
     ],
     imports: [
@@ -27,7 +25,6 @@ import { ContactService } from './components/services/ContactService';
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'contact', component: ContactComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ],
