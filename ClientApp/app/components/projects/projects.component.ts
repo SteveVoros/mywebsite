7ea1f47a63ceb3d858fd6e61229e6980d5287198
stvn100 <31903922+stvn100@users.noreply.github.com>
2017-11-22 16:49:56 -0600

    Projects: Interim save tiles

--- ClientApp/app/components/projects/projects.component.ts
+++ ClientApp/app/components/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AutoScrollAfterLoading } from '../shared/AutoScrollAfterLoading';
 
@@ -8,8 +8,14 @@ import { AutoScrollAfterLoading } from '../shared/AutoScrollAfterLoading';
     styleUrls: ['../shared/shared.css', './projects.component.css']
 })
 export class ProjectsComponent extends AutoScrollAfterLoading {
+    imageUrlTile01: string;
+    imageUrlTile02: string;
+    baseUrl: string;
 
-    constructor(route: ActivatedRoute) {
+    constructor(route: ActivatedRoute, @Inject('BASE_URL') baseUrl: string) {
         super(route);
+        this.baseUrl = baseUrl;
+        this.imageUrlTile01 = this.baseUrl + 'img/tile-cloud.png';
+        this.imageUrlTile02 = this.baseUrl + 'img/tile-cloud-AWS.png';
     }
 }
